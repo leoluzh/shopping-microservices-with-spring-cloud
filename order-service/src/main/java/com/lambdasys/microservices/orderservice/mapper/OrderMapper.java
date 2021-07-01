518c3d0bba0c3e62b9922fd668d9b6a49440fc54
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-07-01 20:38:01 -0300

    Configuração de mapper para ordem e inclusao de mapper itens.

--- order-service/src/main/java/com/lambdasys/microservices/orderservice/mapper/OrderMapper.java
+++ order-service/src/main/java/com/lambdasys/microservices/orderservice/mapper/OrderMapper.java
@@ -1,4 +1,18 @@
 package com.lambdasys.microservices.orderservice.mapper;
 
-public class OrderMapper {
+import com.lambdasys.microservices.orderservice.dto.OrderDto;
+import com.lambdasys.microservices.orderservice.model.Order;
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.MapperConfig;
+
+@MapperConfig(componentModel = "spring", uses = {
+        OrderItemMapper.class
+}, collectionMappingStrategy = CollectionMappingStrategy.ACCESSOR_ONLY
+)
+public interface OrderMapper {
+
+    Order toEntity(OrderDto orderDto);
+
+    OrderDto toDto(Order order);
+
 }
