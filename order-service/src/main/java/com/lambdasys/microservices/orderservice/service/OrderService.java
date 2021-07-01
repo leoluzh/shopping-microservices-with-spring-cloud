d9e6df1248a1b31186bf90fc51a62683789da2e6
Leonardo Fernandes <leonardo.l.fernandes@gmail.com>
2021-07-01 20:45:19 -0300

    Refactoring de mappers.

--- order-service/src/main/java/com/lambdasys/microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/lambdasys/microservices/orderservice/service/OrderService.java
@@ -1,13 +1,16 @@
 package com.lambdasys.microservices.orderservice.service;
 
-import com.lambdasys.microservices.orderservice.mapper.OrderItensMapper;
+import com.lambdasys.microservices.orderservice.dto.OrderDto;
+import com.lambdasys.microservices.orderservice.mapper.OrderItemMapper;
 import com.lambdasys.microservices.orderservice.mapper.OrderMapper;
+import com.lambdasys.microservices.orderservice.model.Order;
 import com.lambdasys.microservices.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
@@ -15,6 +18,13 @@ public class OrderService implements Serializable {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
-    private final OrderItensMapper orderItensMapper;
+    private final OrderItemMapper orderItensMapper;
+
+    public OrderDto create(final OrderDto orderDto){
+       var order = this.orderMapper.toEntity(orderDto);
+        order.setOrderNumber(UUID.randomUUID().toString());
+        this.orderRepository.save(order);
+        return this.orderMapper.toDto(order);
+    }
 
 }
